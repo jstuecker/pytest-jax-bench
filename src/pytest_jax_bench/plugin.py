0fd5e29489838521e5fc0bd77d48c5013b2349b4
Lukas Winkler <git@lw1.at>
2026-01-12 18:09:12 +0100

    allow benchmarking jit compile and memory usage only
    with `jax_bench(jit_rounds=0, jit_warmup=0, eager_rounds=0, eager_warmup=0)`

--- src/pytest_jax_bench/plugin.py
+++ src/pytest_jax_bench/plugin.py
@@ -518,7 +518,7 @@ class JaxBench:
         if fn is not None and (self.eager_rounds > 0 or self.eager_warmup > 0):
             out, res.eager_mean_ms, res.eager_std_ms, res.eager_peak_bytes = self.profile_eager(fn, *args, **kwargs)
 
-        if fn_jit is not None and (self.jit_rounds > 0 or self.jit_warmup > 0):
+        if fn_jit is not None:
             res.compile_ms, lowered, fn_compiled = self.compile_time_ms(fn_jit, *args, **kwargs)
             graph_mem = fn_compiled.memory_analysis()
 
